b2b4ce4b5587886ed7b39e526875a58aad5cb236
Bwiza-Nina <ninamyles2020@gmail.com>
2025-01-12 18:04:38 +0200

    Initial commit for OpenCV assignment

---
+++ process_image.py
@@ -0,0 +1,29 @@
+import cv2
+
+
+
+image = cv2.imread('assign.png')
+
+
+x, y, w, h = 60, 100, 350, 100  
+
+
+cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)  
+
+
+text = "RAH972U"
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 1
+font_thickness = 2
+text_color = (0, 255, 0)  
+text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
+text_x = x
+text_y = y - 10  
+
+cv2.putText(image, text, (text_x, text_y), font, font_scale, text_color, font_thickness)
+
+
+cv2.imwrite('assign_result.png', image)
+
+
+'assign_result.png'
